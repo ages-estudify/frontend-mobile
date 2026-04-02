83d221fb95666b5a09dcfdab7fe740476aa179ec
Ramiro Trejes <ramirotrejes@gmail.com>
2026-04-01 23:00:12 -0300

    feature(86ag41wt2): vscode settings update

--- src/services/questionService.ts
+++ src/services/questionService.ts
@@ -1,46 +1,58 @@
-const BASE_URL = "";
+const BASE_URL = "http://localhost:3000/";
 
 export async function fetchQuestions() {
-  return {
-    data: {
-      questions: [
-        {
-          id: "1",
-          text: "Qual a fórmula química da água?",
-          alternatives: [
-            { label: "A", text: "CO2" },
-            { label: "B", text: "H2O" },
-            { label: "C", text: "HCL" },
-            { label: "D", text: "NaCL" },
-            { label: "E", text: "O2" },
-          ],
+    return {
+        data: {
+            questions: [
+                {
+                    id: "1",
+                    text: "Qual a fórmula química da água?",
+                    alternatives: [
+                        { label: "A", text: "CO2" },
+                        { label: "B", text: "H2O" },
+                        { label: "C", text: "HCl" },
+                        { label: "D", text: "NaCl" },
+                        { label: "E", text: "O2" },
+                    ],
+                },
+                {
+                    id: "2",
+                    text: "Qual o gás que é o maior responsável pelo efeito estufa?",
+                    alternatives: [
+                        { label: "A", text: "Gás Hélio - He" },
+                        { label: "B", text: "Nitrogênio - N2" },
+                        { label: "C", text: "Oxigênio - O2" },
+                        { label: "D", text: "Dióxido de Carbono - CO2" },
+                        { label: "E", text: "Hidrogênio - H2" },
+                    ],
+                },
+                {
+                    id: "3",
+                    text: "Qual o pH aproximado de uma solução neutra a 25°C?",
+                    alternatives: [
+                        { label: "A", text: "2" },
+                        { label: "B", text: "9" },
+                        { label: "C", text: "5" },
+                        { label: "D", text: "13" },
+                        { label: "E", text: "7" },
+                    ],
+                },
+            ],
         },
-        {
-          id: "2",
-          text: "Qual o gás que é o maior responsável pelo efeito estufa?",
-          alternatives: [
-            { label: "A", text: "Gás Hélio - He" },
-            { label: "B", text: "Nitrogênio - N2" },
-            { label: "C", text: "Oxigênio - O2" },
-            { label: "D", text: "Dióxido de Carbono - CO2" },
-            { label: "E", text: "Hidrogênio - H2" },
-          ],
-        },
-        {
-          id: "3",
-          text: "Qual o pH aproximado de uma solução neutra a 25°C?",
-          alternatives: [
-            { label: "A", text: "2" },
-            { label: "B", text: "9" },
-            { label: "C", text: "5" },
-            { label: "D", text: "13" },
-            { label: "E", text: "7" },
-          ],
-        },
-      ],
-    },
-  };
+    };
 }
+
+/*
+export async function fetchQuestions(){
+    fetch(`${BASE_URL}/api/questions/${questionId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ })
+        })
+}
+        */
 
 /*
 export function sendAnswer(questionId: string, answer: string) {
@@ -54,6 +66,7 @@ export function sendAnswer(questionId: string, answer: string) {
 }
     */
 
+
 export function sendAnswer(questionId: string, answer: string) {
-  console.log("Mock:", questionId, answer);
+    console.log("Mock:", questionId, answer);
 }
