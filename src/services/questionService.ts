4c401dda4ba686648910dfbc74d4e4f2d120cfd6
Ramiro Trejes <ramirotrejes@gmail.com>
2026-04-04 17:47:50 -0300

    feature(86ag41wt2): wip: added mock

--- src/services/questionService.ts
+++ src/services/questionService.ts
@@ -1,96 +1,94 @@
+import { FetchQuestionsResponse } from "@/types/Question";
 
 const BASE_URL = "http://localhost:3000";
-
+const USE_MOCK = true;
+let mockCounter = 1;
 
 type FetchParams = {
-    topicId: string;
-    type: "ORIGINAL" | "SIMPLIFIED";
-    limit?: number;
-    excludeAnswered?: boolean;
+  topicId: string;
+  type: "ORIGINAL" | "SIMPLIFIED";
+  limit?: number;
+  excludeAnswered?: boolean;
 };
 
 export async function fetchQuestions({
-    topicId,
-    type,
-    limit = 10,
-    excludeAnswered = true,
-}: FetchParams) {
-    const url = new URL(`${BASE_URL}/api/questions/${topicId}`);
+  topicId,
+  type,
+  limit = 10,
+  excludeAnswered = true,
+}: FetchParams): Promise<FetchQuestionsResponse> {
+  if (USE_MOCK) {
+    await new Promise((r) => setTimeout(r, 500)); // Loading
 
-    url.searchParams.append("type", type);
-    url.searchParams.append("limit", String(limit));
-    url.searchParams.append("excludeAnswered", String(excludeAnswered));
-
-    const response = await fetch(url.toString());
-
-    if (!response.ok) {
-        throw new Error("Erro ao buscar questões")
+    if (mockCounter > 12) {
+      return {
+        data: null,
+        message: "Todas as questões foram respondidas",
+      };
     }
 
-    const data = await response.json();
+    const questions = Array.from({ length: 3 }).map(() => {
+      const id = String(mockCounter++);
 
-    return data;
+      return {
+        id,
+        text: `Pergunta mock ${id}`,
+        type,
+        alternatives: [
+          { label: "A", text: "Opção A" },
+          { label: "B", text: "Opção B" },
+          { label: "C", text: "Opção C" },
+          { label: "D", text: "Opção D" },
+          { label: "E", text: "Opção E" },
+        ],
+      };
+    });
+    return {
+      data: {
+        questions,
+        sessionProgress: {
+          current: Math.min(mockCounter - 1, 12),
+          total: 12,
+        },
+      },
+    };
+  }
+
+  const url = new URL(`${BASE_URL}/api/questions/${topicId}`);
+
+  url.searchParams.append("type", type);
+  url.searchParams.append("limit", String(limit));
+  url.searchParams.append("excludeAnswered", String(excludeAnswered));
+
+  const response = await fetch(url.toString());
+
+  if (!response.ok) {
+    throw new Error("Erro ao buscar questões");
+  }
+
+  const data = await response.json();
+
+  return data;
 }
 
 export async function sendAnswer(questionId: string, answer: string) {
-    const response = await fetch(`${BASE_URL}/api/questions/${questionId}/answer`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ answer }),
-    });
+  if (USE_MOCK) {
+    console.log("Mock enviado: ", questionId, answer);
+    return;
+  }
 
-    if (!response.ok) {
-        throw new Error("Erro ao enviar resposta")
+  const response = await fetch(
+    `${BASE_URL}/api/questions/${questionId}/answer`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ answer }),
     }
-}
+  );
 
-/* MOCK
-return {
-    data: {
-        questions: [
-            {
-                id: "1",
-                text: "Qual a fórmula química da água?",
-                type,
-                alternatives: [
-                    { label: "A", text: "CO2" },
-                    { label: "B", text: "H2O" },
-                    { label: "C", text: "HCl" },
-                    { label: "D", text: "NaCl" },
-                    { label: "E", text: "O2" },
-                ],
-            },
-            {
-                id: "2",
-                text: "Qual o gás que é o maior responsável pelo efeito estufa?",
-                type,
-                alternatives: [
-                    { label: "A", text: "Gás Hélio - He" },
-                    { label: "B", text: "Nitrogênio - N2" },
-                    { label: "C", text: "Oxigênio - O2" },
-                    { label: "D", text: "Dióxido de Carbono - CO2" },
-                    { label: "E", text: "Hidrogênio - H2" },
-                ],
-            },
-            {
-                id: "3",
-                text: "Qual o pH aproximado de uma solução neutra a 25°C?",
-                type,
-                alternatives: [
-                    { label: "A", text: "2" },
-                    { label: "B", text: "9" },
-                    { label: "C", text: "5" },
-                    { label: "D", text: "13" },
-                    { label: "E", text: "7" },
-                ],
-            },
-        ],
-        sessionProgress: {
-            current: 0,
-            total: 20,
-        },
-    },
+  if (!response.ok) {
+    throw new Error("Erro ao enviar resposta");
+  }
 }
-*/
