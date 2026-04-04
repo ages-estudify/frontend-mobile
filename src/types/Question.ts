b74661f7b45537f730d045da8d4413f1faf3dac2
Ramiro Trejes <ramirotrejes@gmail.com>
2026-04-04 16:19:15 -0300

    feature(86ag41wt2): wip: add FetchQuestionsResponse type

--- src/types/Question.ts
+++ src/types/Question.ts
@@ -6,5 +6,18 @@ export type Alternative = {
 export type Question = {
   id: string;
   text: string;
+  imageUrl?: string | null;
+  type: "ORIGINAL" | "SIMPLIFIED";
   alternatives: Alternative[];
 };
+
+export type FetchQuestionsResponse = {
+  data: {
+    questions: Question[];
+    sessionProgress: {
+      current: number;
+      total: number;
+    };
+  } | null;
+  message?: string;
+}
