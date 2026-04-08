2c607c27baaf469b7c9501ddb5f1b5ac097ce359
diego schaeffer <d.schaeffer@edu.pucrs.br>
2026-04-07 21:05:50 -0300

    feature(86ag34uh1): Validating email and requiring a minimum character limit for the password

--- app/register.tsx
+++ app/register.tsx
@@ -230,6 +230,7 @@ export default function RegisterScreen() {
               }}
               className="flex-1"
             />
+
             <Pressable onPress={() => setShowPassword(!showPassword)}>
               <Ionicons
                 name={showPassword ? "eye" : "eye-off"}
