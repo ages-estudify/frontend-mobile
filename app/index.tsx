ac66dd876415b78a867b9125280f95cf7e17ce1d
Isadora Guerra <isa.f.guerra@hotmail.com>
2026-04-07 22:58:18 -0300

    fix: useAuth and AsyncStorage

--- app/index.tsx
+++ app/index.tsx
@@ -1,87 +1,48 @@
-import { ActionButton } from "@/components/ActionButton";
-import { TextInputWithTitle } from "@/components/TextInputWithTitle/TextInputWithTitle";
-import { authService } from "@/services/auth.service";
-import { useRouter } from "expo-router";
-import React, { useState } from "react";
-import { View, Image, Text, Alert } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from "@/hooks/useAuth";
+import { router } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, View, Text, Button } from "react-native";
 
-export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+export default function HomePage() {
+  const [loading, setLoading] = useState(true);
+  const { isAuthenticated } = useAuth();
 
-  const router = useRouter();
+  const { logout } = useAuth();
 
-  const handleLogin = async () => {
-    if (!email || !password) {
-      Alert.alert("Erro", "Preencha todos os campos");
-      return;
-    }
+  useEffect(() => {
+    const checkAuth = async () => {
+      const authenticated = await isAuthenticated();
 
-    setIsLoading(true);
+      if (!authenticated) {
+        router.replace("/login");
+        return;
+      }
 
-    try {
-      const login = await authService.login({
-        email,
-        password,
-      });
+      setLoading(false);
+    };
 
-      router.replace("/progress");
-    } catch (error) {
-      Alert.alert(
-        "Falha no Login",
-        "E-mail ou senha incorretos. Por favor, tente novamente."
-      );
-    } finally {
-      setIsLoading(false);
-    }
-  };
+    checkAuth();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
 
   return (
-    <SafeAreaView className="flex-1">
-      <View className="flex-1 justify-center items-center px-4">
-        <View className="pb-9">
-          <Image
-            source={require("../assets/login_fox.png")}
-            className="w-[159px] h-[132.68px]"
-            resizeMode="contain"
-          />
-          <Text className="text-4xl font-semibold text-purple100">
-            Bem Vindo!
-          </Text>
-        </View>
-
-        <View className="w-full gap-4">
-          <TextInputWithTitle
-            title={"Email"}
-            placeholder={"email@email.com"}
-            onValueChange={setEmail}
-            text={email}
-          />
-          <TextInputWithTitle
-            title={"Senha"}
-            placeholder={"*******"}
-            isPassword={true}
-            onValueChange={setPassword}
-            text={password}
-          />
-        </View>
-
-        <View className="w-full items-center gap-4 pt-16">
-          <ActionButton
-            text={"Login"}
-            action={handleLogin}
-            disabled={isLoading}
-          />
-          <View className="flex-row gap-2">
-            <Text className="text-primaryGray font-regular">
-              Não possui conta?
-            </Text>
-            <Text>Registrar</Text>
-          </View>
-        </View>
-      </View>
-    </SafeAreaView>
+    <View className="flex-1 justify-center items-center">
+      <Text>Home</Text>
+      <Button
+        title="Logout"
+        onPress={async () => {
+          await logout();
+          router.replace("/login");
+        }}
+        color="#007bff"
+      />
+    </View>
   );
 }
--- tests/login.test.tsx
+++ tests/login.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Alert } from "react-native";
 import { fireEvent, render, waitFor } from "@testing-library/react-native";
-import LoginPage from "../app/index";
+import LoginPage from "../app/login";
 
 const mockReplace = jest.fn();
 jest.mock("expo-router", () => ({
