d794106951a22cb24416c37bc58b80a8650d6248
Gabriel <gabrielsdnegre@gmail.com>
2026-04-14 14:13:00 -0300

    fix(0000): endpoint moedas

--- src/services/stars.service.ts
+++ src/services/stars.service.ts
@@ -1,9 +1,9 @@
-import api, { handleApiError } from "./api";
 import type { GetStarsResponse } from "@/types/stars.types";
+import api, { handleApiError } from "./api";
 
 export const getUserStars = async (): Promise<GetStarsResponse> => {
   try {
-    const response: GetStarsResponse = await api.get("/users/coins");
+    const response: GetStarsResponse = await api.get("/users/me/coins");
     return response;
   } catch (error) {
     handleApiError(error);
