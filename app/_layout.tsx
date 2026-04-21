faec95f9f616c8c50a9fc2dff1156c4e0856e4b6
Isadora Guerra <isa.f.guerra@hotmail.com>
2026-04-21 12:41:55 -0300

    fix(86ah0h88h): run format

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,7 +18,6 @@ import {
 
 import { Inter_400Regular, Inter_500Medium, Inter_600SemiBold } from "@expo-google-fonts/inter";
 
-
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
     "Poppins-Regular": Poppins_400Regular,
