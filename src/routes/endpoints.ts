618cdbf841eb3771d3b47496ecb4c89d55e6d83f
szchaeffer3 <reaffeat1406@gmail.com>
2026-06-15 18:52:48 -0300

    feature/FRONT-09.3/fixes

--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -29,6 +29,6 @@ export const endPoints = {
   users: {
     streak: "/users/streak",
     stats: "/users/stats",
-    profilePicture: "/users/me/profile-picture",
+    profilePicture: "/users/profile-picture",
   },
 };
